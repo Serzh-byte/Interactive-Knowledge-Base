95d638afa463e3d012d9b4b5d585b475cee259c6
Serzh-byte <serzhtadevosyan1@gmail.com>
2024-09-13 23:19:49 +0100

    more functionality added

--- knowledge_base/knowledge_base/urls.py
+++ knowledge_base/knowledge_base/urls.py
@@ -18,7 +18,9 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 
+# urls.py
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
+    path("<str:title>", views.entry, name="entry"),
 ]
--- knowledge_base/knowledge_base/views.py
+++ knowledge_base/knowledge_base/views.py
@@ -1,6 +1,20 @@
-from django.shortcuts import render, redirect
+# views.py
+from django.shortcuts import render, redirect, Http404
 from django import forms
+from . import utils
+
 
 def index(request):
     return render(request, "knowledge_base/home.html", {
+        'entries': utils.list_entries()
+    })
+
+
+def entry(request, title):
+    entry_content = utils.get_entry(title)
+    if entry_content is None:
+        raise Http404("Entry not found") #for raising a 404 error
+    return render(request, "knowledge_base/entry.html", {
+        "title": title,
+        "content": entry_content
     })
